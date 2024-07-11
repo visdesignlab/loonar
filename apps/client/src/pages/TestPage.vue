facbb5d7b51d9eb01f6499577c1df9ec8236a4e3
Brian Bollen <briancbollen@gmail.com>
2024-07-11 13:51:54 -0600

    Added duckdb integration and verified that it is working properly.

--- apps/client/src/pages/TestPage.vue
+++ apps/client/src/pages/TestPage.vue
@@ -1,4 +1,4 @@
-<script lang="ts" setup>
+<!-- <script lang="ts" setup>
 import { ref, computed, watch, onMounted } from 'vue';
 
 import { router } from '@/router';
@@ -144,4 +144,94 @@ createCharts();
     ></div>
 </template>
 
-<style scoped></style>
+<style scoped></style> -->
+
+<template>
+    <div v-if="cellMetaData.dataInitialized">
+        <q-item-section style="position: relative">
+            <div
+                id="plotContainer"
+                ref="vgPlotContainer"
+                style="position: relative"
+            ></div>
+        </q-item-section>
+    </div>
+    <div v-else>"HELLOOOO"</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from 'vue';
+import * as vg from '@uwdata/vgplot';
+import { useCellMetaData } from '@/stores/cellMetaData';
+import { useDatasetSelectionStore } from '@/stores/datasetSelectionStore';
+import { storeToRefs } from 'pinia';
+const vgPlotContainer = ref<HTMLDivElement | null>(null);
+const cellMetaData = useCellMetaData();
+const datasetSelectionStore = useDatasetSelectionStore();
+const { dataInitialized } = storeToRefs(cellMetaData);
+const charts = ref<null | HTMLElement>(null);
+async function createCharts() {
+    console.log('created charts');
+    // Configure the coordinator to use DuckDB-WASM
+    // vg.coordinator().databaseConnector(vg.wasmConnector());
+    if (!datasetSelectionStore.currentLocationMetadata) {
+        return;
+    }
+    const url = datasetSelectionStore.getServerUrl(
+        datasetSelectionStore.currentLocationMetadata.tabularDataFilename
+    );
+    console.log('about to load csv');
+    console.log(url);
+    let new_url = url.replace('localhost/data', 'minio:9000/data');
+    await vg
+        .coordinator()
+        .exec([
+            vg.loadCSV(
+                'data',
+                'http://minio:9000/data/TheRealFinalExample/location_0/cells.csv'
+            ),
+        ]);
+    console.log('did we reach here');
+    charts.value = makePlot('mass');
+    if (vgPlotContainer.value) {
+        vgPlotContainer.value.appendChild(charts.value!);
+    }
+}
+watch(dataInitialized, createCharts);
+// createCharts();
+function makePlot(column: string) {
+    const plot = vg.plot(
+        vg.rectY(vg.from('data'), {
+            x: vg.bin(column),
+            y: vg.count(),
+            fill: '#cccccc',
+            inset: 1,
+        }),
+        vg.marginBottom(130),
+        vg.marginTop(30),
+        vg.width(600),
+        vg.height(250),
+        vg.style({ 'font-size': '30px' }),
+        vg.xDomain(vg.Fixed),
+        vg.xLabelAnchor('center'),
+        vg.xTickPadding(10),
+        vg.xLabelOffset(80),
+        vg.xAxis('bottom'),
+        vg.xLine(true),
+        vg.xAlign(0),
+        vg.xInsetRight(20),
+        vg.xTickSpacing(100),
+        vg.yLabelAnchor('top'),
+        vg.yAxis(null),
+        vg.yTicks(0)
+    );
+    return plot;
+}
+</script>
+<style scoped>
+.plot-container {
+    display: flex;
+    align-items: center;
+    text-align: center;
+}
+</style>
