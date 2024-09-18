9d55f2140b12997fee3641da98b7b04ba6104747
Devin Lange <u1265145@utah.edu>
2024-09-18 10:25:35 -0600

    Update apps/client/src/util/decoder.worker.ts
    Co-authored-by: bbollen23 <briancbollen@gmail.com>

--- apps/client/src/util/decoder.worker.ts
+++ apps/client/src/util/decoder.worker.ts
@@ -8,10 +8,30 @@ addDecoder(50000, () => ZstdDecoder);
 // @ts-expect-error - We are in a worker context
 const worker: ServiceWorker = self;
 
-worker.addEventListener('message', async (e) => {
-    // @ts-expect-error - FIXME: we should have strict types
-    const { id, fileDirectory, buffer } = e.data;
-    const decoder = await getDecoder(fileDirectory);
-    const decoded = await decoder.decode(fileDirectory, buffer);
-    worker.postMessage({ decoded, id }, [decoded]);
+interface WorkerData {
+    id: number;
+    fileDirectory: string;
+    buffer: ArrayBuffer;
+}
+
+async function decodeBuffer(e: MessageEvent<WorkerData>): Promise<void> {
+    try {
+        const { id, fileDirectory, buffer } = e.data;
+        const decoder = await getDecoder(fileDirectory);
+        const decoded = await decoder.decode(fileDirectory, buffer);
+        worker.postMessage({ decoded, id }, [decoded]);
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`Failed: ${error.message} at ${e.data.id}`);
+        } else {
+            console.error(`Failed: Generic Error at ${e.data.id}`);
+        }
+    }
+}
+
+worker.addEventListener('message', (event: Event) => {
+    const messageEvent = event as MessageEvent;
+    decodeBuffer(messageEvent).catch((error: Error) => {
+        console.error(`Error during decompression: ${error.message}`)
+    });
 });
