739144474a7a25222e7ca795a126fb7bf0047111
Luke-Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2024-10-09 10:46:57 -0600

    Condition Selector moved to folder, condition chart component added

---
+++ apps/client/src/components/conditionSelector/ConditionChart.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useGlobalSettings } from '@/stores/componentStores/globalSettingsStore';
+const globalSettings = useGlobalSettings();
+</script>
+
+<template>
+    <div></div>
+</template>
+
+<style></style>
