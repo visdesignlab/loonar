7fb1fef2778523aa2f554eacc231dc9ec5717a86
Luke-Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2024-10-28 18:46:20 -0600

    Shaded area of chart colors altered

--- apps/client/src/components/conditionSelector/ConditionChart.vue
+++ apps/client/src/components/conditionSelector/ConditionChart.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, nextTick} from 'vue';
+import { ref, nextTick } from 'vue';
 import { useGlobalSettings } from '@/stores/componentStores/globalSettingsStore';
 import { useDatasetSelectionStore } from '@/stores/dataStores/datasetSelectionUntrrackedStore';
 import { useMosaicSelectionStore } from '@/stores/dataStores/mosaicSelectionStore';
@@ -12,50 +12,46 @@ import { storeToRefs } from 'pinia';
 // Props from parent component
 // Accept props from the parent component
 const props = defineProps<{
-    tags: [string,string][];
+    tags: [string, string][];
     xAxisName: string;
     yAxisName: string;
-    yIndex:number;
-    selected:boolean;
+    yIndex: number;
+    selected: boolean;
 }>();
 
 // Will use soon for dark mode.
 const globalSettings = useGlobalSettings();
 
 const datasetSelectionStore = useDatasetSelectionStore();
-const { experimentDataInitialized, currentExperimentMetadata } = storeToRefs(datasetSelectionStore);
-const { conditionChartSelections  } = useMosaicSelectionStore();
+const { experimentDataInitialized, currentExperimentMetadata } = storeToRefs(
+    datasetSelectionStore
+);
+const { conditionChartSelections } = useMosaicSelectionStore();
 const { chartColorScheme } = useConditionSelectorStore();
 
 // Container for chart.
 const chartContainer = ref<HTMLElement | null>(null);
-watch([experimentDataInitialized, conditionChartSelections], async ([isInitialized, newConditionChartSelections]) => {    
-    if(isInitialized){
-        await nextTick(); // Helps with hot reloading. On save, html ref will be temporarily none. This will wait until html has a ref.
-        createChart(
-            props.xAxisName,
-            props.yAxisName,
-        );
-    }
-}, {immediate : true, deep : true })
-
+watch(
+    [experimentDataInitialized, conditionChartSelections],
+    async ([isInitialized, newConditionChartSelections]) => {
+        if (isInitialized) {
+            await nextTick(); // Helps with hot reloading. On save, html ref will be temporarily none. This will wait until html has a ref.
+            createChart(props.xAxisName, props.yAxisName);
+        }
+    },
+    { immediate: true, deep: true }
+);
 
 // Creates a new set of selections and adds them to an overall list that is updated whenever general mosaicSelection is updated.
 // Returns the "base" and "filtered". Base a constant selection which is never updated. This is only to filter based on the tags. The "filteredSelection" is updated by other general filters (i.e. mosaicSelection).
 
 // Styles
-const lineColor = chartColorScheme[props.yIndex % 6]
+const lineColor = chartColorScheme[props.yIndex % 6];
 const strokeWidth = 5;
 
-async function createChart(
-    xAxisName: string,
-    yAxisName: string,
-) {
-
-
-    if (chartContainer.value ) {
-
-        const source = `${props.tags[0][0]}-${props.tags[0][1]}_${props.tags[1][0]}-${props.tags[1][1]}`
+async function createChart(xAxisName: string, yAxisName: string) {
+    if (chartContainer.value) {
+        const source = `${props.tags[0][0]}-${props.tags[0][1]}_${props.tags[1][0]}-${props.tags[1][1]}`;
         // Creates chart, filtered by the selection that uses the query.
         const chart = vg.plot(
             // Fills in area under line chart grey (optional)
@@ -63,7 +59,44 @@ async function createChart(
                 vg.from(
                     `${currentExperimentMetadata?.value?.name}_composite_experiment_cell_metadata`,
                     {
-                        filterBy: conditionChartSelections[source].baseSelection
+                        filterBy:
+                            conditionChartSelections[source].baseSelection,
+                    }
+                ),
+                {
+                    x: xAxisName,
+                    y1: 0,
+                    y: vg.avg(yAxisName),
+                    fill: '#f8f8f8',
+                    fillOpacity: 1,
+                    curve: 'basis',
+                    stroke: null,
+                }
+            ),
+            vg.lineY(
+                vg.from(
+                    `${currentExperimentMetadata?.value?.name}_composite_experiment_cell_metadata`,
+                    {
+                        filterBy:
+                            conditionChartSelections[source].baseSelection,
+                    }
+                ),
+                {
+                    x: xAxisName,
+                    y: vg.avg(yAxisName),
+                    stroke: lineColor,
+                    strokeWidth: strokeWidth,
+                    curve: 'basis',
+                    opacity: 0.2,
+                }
+            ),
+            // Plots Line Chart based on selection.
+            vg.areaY(
+                vg.from(
+                    `${currentExperimentMetadata?.value?.name}_composite_experiment_cell_metadata`,
+                    {
+                        filterBy:
+                            conditionChartSelections[source].filteredSelection,
                     }
                 ),
                 {
@@ -80,7 +113,8 @@ async function createChart(
                 vg.from(
                     `${currentExperimentMetadata?.value?.name}_composite_experiment_cell_metadata`,
                     {
-                        filterBy: conditionChartSelections[source].baseSelection
+                        filterBy:
+                            conditionChartSelections[source].filteredSelection,
                     }
                 ),
                 {
@@ -89,24 +123,7 @@ async function createChart(
                     stroke: lineColor,
                     strokeWidth: strokeWidth,
                     curve: 'basis',
-                    opacity:0.5
-                }
-            ),
-            // Plots Line Chart based on selection.
-            vg.lineY(
-                vg.from(
-                    `${currentExperimentMetadata?.value?.name}_composite_experiment_cell_metadata`,
-                    {
-                        filterBy: conditionChartSelections[source].filteredSelection
-                    }
-                ),
-                {
-                    x: xAxisName,
-                    y: vg.avg(yAxisName),
-                    stroke: lineColor,
-                    strokeWidth: strokeWidth,
-                    curve: 'basis',
-                    opacity:1
+                    opacity: 1,
                 }
             ),
 
