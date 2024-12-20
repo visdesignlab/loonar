3c6b741c548a0a6c8503a46bcddaae5460633bf5
dev-lan <devinscottlange@gmail.com>
2024-12-20 15:20:08 -0500

    make problem/warning notifications persistent, hide info/success notifications

--- apps/client/src/stores/misc/notificationStore.ts
+++ apps/client/src/stores/misc/notificationStore.ts
@@ -17,6 +17,14 @@ interface QNotification {
 export const useNotificationStore = defineStore('notificationStore', () => {
     const $q = useQuasar();
 
+    // controls which types of notifications are displayed
+    const displayLevels: NotificationType[] = [
+        'problem',
+        'warning',
+        // 'success', // uncommont these to rurn these back on.
+        // 'info',
+    ];
+
     const typeToQNotify: Record<NotificationType, QNotification> = {
         problem: {
             color: 'negative',
@@ -41,9 +49,37 @@ export const useNotificationStore = defineStore('notificationStore', () => {
     };
 
     function notify({ message, type }: Notification) {
+        if (!displayLevels.includes(type)) {
+            return;
+        }
+        let copyProps = {};
+        if (type === 'problem' || type === 'warning') {
+            copyProps = {
+                timeout: 0,
+                actions: [
+                    {
+                        label: 'Copy Message',
+                        color: type === 'warning' ? 'black' : 'white',
+                        noDismiss: true,
+                        handler: () => {
+                            navigator.clipboard.writeText(message);
+                        },
+                    },
+                    {
+                        label: 'dismiss',
+                        color: type === 'warning' ? 'black' : 'white',
+                        handler: () => {
+                            /* no action needed other than closing */
+                        },
+                    },
+                ],
+            };
+        }
         $q.notify({
             ...typeToQNotify[type],
+            textColor: type === 'warning' ? 'black' : 'white',
             message,
+            ...copyProps,
         });
     }
 
