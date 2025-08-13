2164f0770e756d458352279fb496a5326cd0a375
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-08-13 14:17:00 -0600

    Refactor: Rename css & layout without changing appearance

--- apps/docs-website/src/pages/index.tsx
+++ apps/docs-website/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 import React from "react";
 import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import styles from "./index.module.css";
 import { demoURL } from "./demoUrl";
@@ -13,26 +12,28 @@ export default function Home(): JSX.Element {
   return (
     <Layout>
       <div className={styles.pageWrapper}>
-        <header className={clsx("hero hero--secondary", styles.heroBanner)}>
-          <video
-            className={styles.heroVideo}
-            autoPlay
-            muted
-            loop
-            playsInline
-            preload="auto"
-          >
-            <source src="/videos/loon_demo_video_1.mp4" type="video/mp4" />
-            {/* Fallback text */}
-            Your browser does not support the video tag.
-          </video>
+        <section className={clsx(styles.heroSection)}>
+          <div className={styles.heroVideoWrapper}>
+            <video
+              className={styles.heroVideo}
+              autoPlay
+              muted
+              loop
+              playsInline
+              preload="auto"
+            >
+              <source src="/videos/loon_demo_video_1.mp4" type="video/mp4" />
+              {/* Fallback text */}
+              Your browser does not support the video tag.
+            </video>
+          </div>
           <div className={styles.heroContent}>
-            <div className={styles.titleContainer}>
-              <div className={clsx("hero__title", styles.headline)}>
+            <header className={styles.heroHeader}>
+              <h1 className={styles.headline}>
                 <span className={styles.headlineLine}>Data Visualization</span>
                 <span className={styles.headlineLine}>for Live Cell Microscopy</span>
-              </div>
-            </div>
+              </h1>
+            </header>
             <div className={styles.buttonRow}>
               <Link
                 className="button button--primary button--lg"
@@ -48,7 +49,7 @@ export default function Home(): JSX.Element {
               </Link>
             </div>
           </div>
-        </header>
+        </section>
       </div>
     </Layout>
   );
