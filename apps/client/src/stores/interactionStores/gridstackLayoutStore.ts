f578f5cd36f06d5debe019665b890fd90ce01532
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2025-12-09 11:58:19 -0700

    Default layout changes if more than 20 locations

--- apps/client/src/stores/interactionStores/gridstackLayoutStore.ts
+++ apps/client/src/stores/interactionStores/gridstackLayoutStore.ts
@@ -3,6 +3,7 @@ import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 import { v4 as uuidv4 } from 'uuid';
 import { cloneDeep } from 'lodash-es';
+import { useDatasetSelectionStore } from '../dataStores/datasetSelectionUntrrackedStore';
 export interface LayoutItem {
     // gridstack properties plus my own
     component: string;
@@ -262,166 +263,172 @@ export const useGridstackLayoutStore = defineStore(
         //     },
         // ];
 
-        const allEqualItems: LayoutItem[] = [
-            {
-                component: 'ConditionSelector',
-                displayName: 'Condition Selector',
-                h: 20,
-                icon: 'apps',
-                id: 'ConditionSelector',
-                noPadding: true,
-                w: 6,
-                x: 0,
-                y: 0,
-            },
-            {
-                component: 'ExemplarView',
-                displayName: 'Exemplar View',
-                h: 10,
-                hideOverflow: true,
-                icon: 'location_on',
-                id: 'ExemplarView',
-                noPadding: true,
-                sidebar: 'ExemplarViewSettingsSidebar',
-                toolbar: 'ExemplarViewSettingsToolbar',
-                w: 6,
-                x: 6,
-                y: 0,
-            },
-            {
-                component: 'LooneageViewGL',
-                displayName: 'Looneage',
-                h: 10,
-                hideOverflow: true,
-                icon: 'account_tree',
-                id: 'LooneageViewGL',
-                noPadding: true,
-                sidebar: 'LooneageViewSettingsSidebar',
-                toolbar: 'LooneageViewSettingsToolbar',
-                w: 6,
-                x: 0,
-                y: 20,
-            },
-            {
-                component: 'AggregateLineChart',
-                displayName: 'Line Chart',
-                h: 10,
-                icon: 'timeline',
-                id: 'AggregateLineChart',
-                noPadding: true,
-                sidebar: 'AggregateLineChartSettingsSidebar',
-                toolbar: 'AggregateLineChartSettingsToolbar',
-                w: 6,
-                x: 6,
-                y: 10,
-            },
-            {
-                component: 'SimpleTable',
-                displayName: 'Lineages',
-                h: 10,
-                icon: 'table_chart',
-                id: 'SimpleTable-lineage',
-                noPadding: true,
-                props: {
-                    attributeLevel: 'lineage',
+        const datasetSelectionStore = useDatasetSelectionStore();
+
+        const allEqualItems = computed<LayoutItem[]>(() => {
+            const items: LayoutItem[] = [
+                {
+                    component: 'ConditionSelector',
+                    displayName: 'Condition Selector',
+                    h: 20,
+                    icon: 'apps',
+                    id: 'ConditionSelector',
+                    noPadding: true,
+                    w: 6,
+                    x: 0,
+                    y: 10,
                 },
-                w: 6,
-                x: 0,
-                y: 30,
-            },
-            {
-                component: 'ImageViewer',
-                displayName: 'Images',
-                h: 10,
-                hideOverflow: true,
-                icon: 'image',
-                id: 'ImageViewer',
-                noPadding: true,
-                sidebar: 'ImageViewerSettingsSidebar',
-                toolbar: 'ImageViewerSettingsToolbar',
-                w: 6,
-                x: 6,
-                y: 20,
-            },
-            {
-                component: 'SimpleTable',
-                displayName: 'Tracks',
-                h: 10,
-                icon: 'table_chart',
-                id: 'SimpleTable-track',
-                noPadding: true,
-                props: {
-                    attributeLevel: 'track',
+                {
+                    component: 'ExemplarView',
+                    displayName: 'Exemplar View',
+                    h: 10,
+                    hideOverflow: true,
+                    icon: 'location_on',
+                    id: 'ExemplarView',
+                    noPadding: true,
+                    sidebar: 'ExemplarViewSettingsSidebar',
+                    toolbar: 'ExemplarViewSettingsToolbar',
+                    w: 6,
+                    x: 6,
+                    y: 0,
                 },
-                w: 6,
-                x: 0,
-                y: 40,
-            },
-            {
-                component: 'SimpleTable',
-                displayName: 'Cells',
-                h: 10,
-                icon: 'table_chart',
-                id: 'SimpleTable-cell',
-                noPadding: true,
-                props: {
-                    attributeLevel: 'cell',
+                {
+                    component: 'LooneageViewGL',
+                    displayName: 'Looneage',
+                    h: 10,
+                    hideOverflow: true,
+                    icon: 'account_tree',
+                    id: 'LooneageViewGL',
+                    noPadding: true,
+                    sidebar: 'LooneageViewSettingsSidebar',
+                    toolbar: 'LooneageViewSettingsToolbar',
+                    w: 6,
+                    x: 0,
+                    y: 0,
                 },
-                w: 6,
-                x: 6,
-                y: 30,
-            },
-            {
-                component: 'TrrackVisWrapper',
-                displayName: 'History',
-                h: 10,
-                icon: 'history',
-                id: 'TrrackVisWrapper',
-                noPadding: true,
-                w: 6,
-                x: 0,
-                y: 50,
-            },
-        ];
+                {
+                    component: 'AggregateLineChart',
+                    displayName: 'Line Chart',
+                    h: 10,
+                    icon: 'timeline',
+                    id: 'AggregateLineChart',
+                    noPadding: true,
+                    sidebar: 'AggregateLineChartSettingsSidebar',
+                    toolbar: 'AggregateLineChartSettingsToolbar',
+                    w: 6,
+                    x: 6,
+                    y: 10,
+                },
+                {
+                    component: 'SimpleTable',
+                    displayName: 'Lineages',
+                    h: 10,
+                    icon: 'table_chart',
+                    id: 'SimpleTable-lineage',
+                    noPadding: true,
+                    props: {
+                        attributeLevel: 'lineage',
+                    },
+                    w: 6,
+                    x: 0,
+                    y: 30,
+                },
+                {
+                    component: 'ImageViewer',
+                    displayName: 'Images',
+                    h: 10,
+                    hideOverflow: true,
+                    icon: 'image',
+                    id: 'ImageViewer',
+                    noPadding: true,
+                    sidebar: 'ImageViewerSettingsSidebar',
+                    toolbar: 'ImageViewerSettingsToolbar',
+                    w: 6,
+                    x: 6,
+                    y: 20,
+                },
+                {
+                    component: 'SimpleTable',
+                    displayName: 'Tracks',
+                    h: 10,
+                    icon: 'table_chart',
+                    id: 'SimpleTable-track',
+                    noPadding: true,
+                    props: {
+                        attributeLevel: 'track',
+                    },
+                    w: 6,
+                    x: 0,
+                    y: 40,
+                },
+                {
+                    component: 'SimpleTable',
+                    displayName: 'Cells',
+                    h: 10,
+                    icon: 'table_chart',
+                    id: 'SimpleTable-cell',
+                    noPadding: true,
+                    props: {
+                        attributeLevel: 'cell',
+                    },
+                    w: 6,
+                    x: 6,
+                    y: 30,
+                },
+                {
+                    component: 'TrrackVisWrapper',
+                    displayName: 'History',
+                    h: 10,
+                    icon: 'history',
+                    id: 'TrrackVisWrapper',
+                    noPadding: true,
+                    w: 6,
+                    x: 0,
+                    y: 50,
+                },
+            ];
+
+            const locationCount =
+                datasetSelectionStore.currentExperimentMetadata
+                    ?.locationMetadataList.length ?? 0;
+
+            if (locationCount > 20) {
+                const conditionSelector = items.find(
+                    (item) => item.id === 'ConditionSelector'
+                );
+                const looneageView = items.find(
+                    (item) => item.id === 'LooneageViewGL'
+                );
+
+                if (conditionSelector && looneageView) {
+                    conditionSelector.y = 0;
+                    looneageView.y = 20;
+                }
+            }
+
+            return items;
+        });
 
         const defaultId = 'system_layout_0';
 
-        // const smallImageLayout: Layout = {
-        //     name: 'Small Image',
-        //     editable: false,
-        //     editing: false,
-        //     id: defaultId,
-        //     initialItems: cloneDeep(smallImageItems),
-        //     currentItems: cloneDeep(smallImageItems),
-        // };
-        // const largeImageLayout: Layout = {
-        //     name: 'Large Image',
-        //     editable: false,
-        //     editing: false,
-        //     id: 'system_layout_1',
-        //     initialItems: cloneDeep(largeImageItems),
-        //     currentItems: cloneDeep(largeImageItems),
-        // };
-
-        const allEqualLayout: Layout = {
+        const allEqualLayout = computed<Layout>(() => ({
             name: 'Default',
             editable: false,
             editing: false,
             id: defaultId,
-            initialItems: cloneDeep(allEqualItems),
-            currentItems: cloneDeep(allEqualItems),
-        };
-        console.log('Default layout object:', allEqualLayout);
+            initialItems: cloneDeep(allEqualItems.value),
+            currentItems: cloneDeep(allEqualItems.value),
+        }));
+
+        // console.log('Default layout object:', allEqualLayout.value);
 
         const currentLayoutId = useStorage<string>(
             'currentLayoutId',
             defaultId
         );
 
-        const systemLayoutOptions = ref<Layout[]>([
-            // smallImageLayout,
-            // largeImageLayout,
-            allEqualLayout,
+        const systemLayoutOptions = computed<Layout[]>(() => [
+            allEqualLayout.value,
         ]);
         const userLayoutOptions = useStorage<Layout[]>('userLayoutOptions', []);
         const allLayouts = computed<Map<string, Layout>>(() => {
