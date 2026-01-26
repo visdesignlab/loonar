b2f26391a5e71b6f33179ec8ece4f95da0c9b3ac
Luke Schreiber <131496683+Luke-Schreiber@users.noreply.github.com>
2026-01-26 09:27:40 -0700

    Add explanatory commants to initialization function

--- apps/client/src/stores/componentStores/imageViewerUntrrackedStore.ts
+++ apps/client/src/stores/componentStores/imageViewerUntrrackedStore.ts
@@ -6,24 +6,29 @@ export const useImageViewerStoreUntrracked = defineStore(
     'imageViewerStoreUntrracked',
     () => {
         const datasetSelectionTrrackedStore = useDatasetSelectionTrrackedStore();
+        // Get the current experiment filename.
         const { currentExperimentFilename } = storeToRefs(
             datasetSelectionTrrackedStore
         );
+        // Initialize the contrast limit slider to 0, 0.
         const contrastLimitSlider = ref<{ min: number; max: number }>({
             min: 0,
             max: 0,
         });
 
+        // Reset the contrast limit slider when the experiment filename changes.
         watch(currentExperimentFilename, () => {
             contrastLimitSlider.value = { min: 0, max: 0 };
         });
 
+        // Initialize the size variables to 1.
         const sizeX = ref<number>(1);
         const sizeY = ref<number>(1);
         const sizeT = ref<number>(1);
         const sizeC = ref<number>(1);
 
         function initializeContrastLimits(limits: [number, number]) {
+            // If the contrast limit slider is not initialized (0, 0), initialize it
             if (
                 contrastLimitSlider.value.min === 0 &&
                 contrastLimitSlider.value.max === 0
